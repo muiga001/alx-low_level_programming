835ed59ab6732d98e195929673d77f528edb036f
muiga001 <muigakenan@gmail.com>
2023-09-11 23:19:46 +0300

    print 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <main>
+
+/**
+ * main - Entry point
+ *
+ * Return: zero on success
+ */
+int main(void)
+{
+	int namba;
+
+	for (namba = 0; namba <= 9; namba++)
+		printf("%i", namba);
+	printf("\n");
+	return (0);
+}
