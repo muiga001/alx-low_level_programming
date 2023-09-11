a25a502a750a41d489da759fdc1740eab3bfd1dd
muiga001 <muigakenan@gmail.com>
2023-09-12 00:07:17 +0300

    combine numbers and spaces

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Zero on success
+ */
+int main(void)
+{
+	int j;
+
+	for (j = 0; j <= 9, j++)
+	{
+		putchar(j + '0');
+		if (j != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
