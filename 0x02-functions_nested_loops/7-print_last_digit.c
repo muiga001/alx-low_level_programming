3f90987a1fa5a7d17dfff4d71856984c1b87839c
muiga001 <muigakenan@gmail.com>
2023-09-14 04:30:17 -0400

    the last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - give last digit
+ *
+ * @l: int given
+ * Return: the last digit
+ */
+int print_last_digit(int l)
+{
+	int ret;
+
+	ret = l % 10;
+	return (ret);
+}
