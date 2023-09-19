3b51b2c76edf4fe1e1da668f8fe47b5caf63e299
muiga001 <muigakenan@gmail.com>
2023-09-19 15:34:08 -0400

    second half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * puts_half - prints second half
+ * @str: pointer to string
+ *
+ * Return: null
+ */
+void puts_half(char *str)
+{
+	int len;
+	int index;
+
+	len = 0;
+
+	while (str[len] != '\0')
+		len++;
+
+	if ((len % 2) != 0)
+		index = ((len - 1) / 2);
+	else
+		index = (len / 2);
+
+	while (str[index] != '\0')
+	{
+		_putchar(str[index]);
+		str++;
+	}
+	_putchar('\n');
+}
