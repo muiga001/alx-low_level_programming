d6384124ae962281f377089c2ddfeb758a4a54f6
muiga001 <muigakenan@gmail.com>
2023-09-28 16:35:19 -0400

    string length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - string length
+ * @s: string
+ * Return: string length
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	s++;
+	return (_strlen_recursion(s) + 1);
+}
