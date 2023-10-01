be6e6cdf7fbb933f15bcdda4a016f406f3371c27
muiga001 <muigakenan@gmail.com>
2023-10-01 14:51:25 -0400

    argc[0]

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: count
+ * @argv: vector
+ * Return: zero on success
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
