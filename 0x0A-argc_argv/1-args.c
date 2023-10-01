625ade72201251d72ace485000ee3a6e48f45a9a
muiga001 <muigakenan@gmail.com>
2023-10-01 14:59:56 -0400

    count arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: count
+ * @argv: vector
+ * Return: zero on success
+ */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", (argc - 1));
+	return (0);
+}
