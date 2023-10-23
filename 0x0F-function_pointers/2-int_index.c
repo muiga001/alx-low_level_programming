46f272638ff41f1840de381db4765484c5222a60
muiga001 <muigakenan@gmail.com>
2023-10-23 20:35:46 +0000

    complete function pointers

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include "function_pointers.h"
+/**
+ * int_index - searches for an integer
+ * @array: blank
+ * @size: blank
+ * @cmp: blank
+ *
+ * Return: void
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size > 0)
+	{
+		if (array != NULL && cmp != NULL)
+		{
+			while (i < size)
+			{
+				if (cmp(array[i]))
+					return (i);
+
+				i++;
+			}
+		}
+	}
+
+	return (-1);
+}
